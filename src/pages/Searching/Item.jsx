f69ab06c305bcfc40b63867839ed99f69dcf4b6b
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-03-20 23:28:03 -0600

    parte 4

--- src/pages/Searching/Item.jsx
+++ src/pages/Searching/Item.jsx
@@ -7,14 +7,14 @@ function Item({ item }) {
   const [fondo, setFondo] = useState(false);
   return (
     <div className="w-full h-[250px] items-center text-center relative">
-      {item.mainImage && item.mainImage.asset ? (
+      {item?.mainImage && item?.mainImage.asset ? (
         <img
           src={urlFor(item?.mainImage.asset._ref)}
           className={`w-full h-full object-cover  absolute `}
           onMouseOver={() => setFondo(true)}
         />
       ) : null}
-      {item.image && item.image.length > 0 ? (
+      {item?.image && item?.image.length > 0 ? (
         <img
           src={urlFor(item?.image[0].asset._ref)}
           className={`w-full h-full object-contain  absolute `}
@@ -34,15 +34,15 @@ function Item({ item }) {
         <div className="flex flex-col items-center text-center justify-center w-full h-full text-white">
           <h2 className="text-xl font-bold">{item?.title}</h2>
           <h2 className="text-xl font-bold">{item?.name}</h2>
-          {item.mainImage ? (
+          {item?.mainImage ? (
             <span className="flex flex-row  uppercase font-bold lg:font-medium cursor-pointer mt-0 lg:mt-4 hover:underline">
-              <Link href={`/Post/${item.slug.current} `}>read more &nbsp;</Link>
+              <Link href={`/Post/${item?.slug.current} `}>read more &nbsp;</Link>
               <HiArrowUpRight className=" mt-2 text-xs" />
             </span>
           ) : null}
-          {item.image && item.image.length > 0 ? (
+          {item?.image && item?.image.length > 0 ? (
             <span className="flex flex-row  uppercase font-bold lg:font-medium cursor-pointer mt-0 lg:mt-4 hover:underline">
-              <Link href={`/Product/${item.slug.current} `}>
+              <Link href={`/Product/${item?.slug.current} `}>
                 see more &nbsp;
               </Link>
               <HiArrowUpRight className=" mt-2 text-xs" />
